10a1a0240c8045bccff989d2a6339f6cc9f970bb
poncegamez1 <poncegamez1@gmail.com>
2022-12-20 15:55:14 -0500

    Commit Inicial.

---
+++ app/src/main/java/com/poncegamez/marvelapp_pt_tapes/list/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.poncegamez.marvelapp_pt_tapes.list
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/poncegamez/marvelapp_pt_tapes/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.poncegamez.marvelapp_pt_tapes.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.poncegamez.marvelapp_pt_tapes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
